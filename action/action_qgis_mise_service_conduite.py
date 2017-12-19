33a0774c806c8f1de01486085567e302626b160a
Savary <daniel.savary@lausanne.ch>
2017-12-19 15:20:51 +0100

    utilisation des identifiantes de couche du projet layer.id()
    Un contrôle est ajouté pour savoir si l'identifiant de la couche
    concernée est toujours existant dans le projet QGIS. Avertissement à
    l'utilisateur. La couche en question ne sera pas prise en compte par
    conséquent.

--- action/action_qgis_mise_service_conduite.py
+++ action/action_qgis_mise_service_conduite.py
@@ -7,11 +7,14 @@ from PyQt4.QtGui import QDialog,QMessageBox
 
 global featureId
 featureId = sys.var[0]
+# liste des identifiants des couches du projet oû l'archivage est possible / Attention à la structure du projet si une couche est rajoutée par la suite dans le projet
+layerIds = ["valve20130304110011497" , "od_part20140429113327995","hydrant20130304110004848"]
 
 def updateField():
     maCouche = iface.activeLayer()
     layerName = maCouche.name()
     formTitle = layerName + u" - Attributs d'entités"
+    layers = QgsMapLayerRegistry.instance().mapLayers() # liste des couches du projet
 
     for w in iface.mainWindow().findChildren(QDialog):
         if w.windowTitle() == formTitle and w.isActiveWindow():
@@ -44,10 +47,16 @@ def updateField():
                 num_v = 0
                 num_h = 0
                 num_i = 0
-                for layer in QgsMapLayerRegistry.instance().mapLayers().values():
+                for i in range(0,len(layerIds)):
+                        if layerIds[i] not in layers:
+                            text_layer = u"La couche avec ID = "+ str(layerIds[i]) + u" n'existe plus ou pas dans le projet, merci de vérifier la structure du projet sinon elle ne sera pas prise en compte dans la mise en service"
+                            msgBox.setText(text_layer)
+                            msgBox.exec_()
+                for layer in layers.values():
                     uri = QgsDataSourceURI(layer.source())
                     name = uri.schema() + "." + uri.table()
-                    if name in ["qwat_od.valve", "qwat_od.part" , "qwat_od.hydrant", "qwat_od.subscriber"]:
+                    #if name in ["qwat_od.valve", "qwat_od.part" , "qwat_od.hydrant", "qwat_od.subscriber"]:
+                    if layer.id() in layerIds:
                         num_elements = 0
                         if not layer.isEditable():
                             layer.startEditing()
